e1ae21462a971b75938a3a1cdecfcaae8489e324
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-03 17:53:37 +0900

    Post defaultValues と object components化

--- src/features/post/Post.jsx
+++ src/features/post/Post.jsx
@@ -8,6 +8,9 @@ import * as postSlice from "./postSlice";
 import { Header } from "./components/header/Header";
 import { Main } from "./components/main/Main";
 
+import * as matters from "./matters/defaultValues";
+import * as resources from "./resources/defaultValues";
+
 export const Post = ({ index, handleClose }) => {
   const dispatch = useDispatch();
 
@@ -16,187 +19,15 @@ export const Post = ({ index, handleClose }) => {
   const methods = useForm({
     defaultValues:
       index === "matters"
-        ? {
-            display: post?.display,
-            title: post?.title,
-            body: post?.body,
-            position: post?.position,
-            location: post?.location,
-            period: {
-              year: post?.period?.year,
-              month: post?.period?.month,
-            },
-            costs: {
-              min: post?.costs?.min,
-              max: post?.costs?.max,
-              contract: post?.costs?.contract,
-              display: post?.costs?.display,
-              type: post?.costs?.type,
-            },
-            handles: post.handles?.[0]
-              ? post.handles.map((value) => ({
-                  handle: value,
-                }))
-              : [{ handle: "" }],
-            tools: post.tools?.[0]
-              ? post.tools.map((value) => ({
-                  tool: value,
-                }))
-              : [{ tool: "" }],
-            requires: post.requires?.[0]
-              ? post.requires.map((value) => ({
-                  require: value,
-                }))
-              : [{ require: "" }],
-            prefers: post.prefers?.[0]
-              ? post.prefers.map((value) => ({
-                  prefer: value,
-                }))
-              : [{ prefer: "" }],
-            adjustment: post?.adjustment,
-            interviews: post?.interviews,
-            times: post?.times,
-            remote: post?.remote,
-            distribution: post?.distribution,
-            span: post?.span,
-            note: post?.note,
-            status: post?.status,
-            memo: post?.memo,
-          }
-        : index === "resources" && {
-            display: post?.display,
-            name: post?.name,
-            roman: post?.roman,
-            position: post?.position,
-            sex: post?.sex,
-            age: post?.age,
-            body: post?.body,
-            belong: post?.belong,
-            station: post?.station,
-            period: {
-              year: post?.period?.year,
-              month: post?.period?.month,
-            },
-            costs: {
-              min: post?.costs?.min,
-              max: post?.costs?.max,
-              contract: post?.costs?.contract,
-              display: post?.costs?.display,
-              type: post?.costs?.type,
-            },
-            handles: post.handles?.[0]
-              ? post.handles.map((value) => ({
-                  handle: value,
-                }))
-              : [{ handle: "" }],
-            tools: post.tools?.[0]
-              ? post.tools.map((value) => ({
-                  tool: value,
-                }))
-              : [{ tool: "" }],
-            skills: post.skills?.[0]
-              ? post.skills.map((value) => ({
-                  skill: value,
-                }))
-              : [{ skill: "" }],
-            parallel: post?.parallel,
-            note: post?.note,
-            status: post?.status,
-            memo: post?.memo,
-          },
+        ? matters.defaultValues(post)
+        : index === "resources" && resources.defaultValues(post),
   });
 
   useEffect(() => {
     methods.reset(
       index === "matters"
-        ? {
-            display: post?.display,
-            title: post?.title,
-            body: post?.body,
-            position: post?.position,
-            location: post?.location,
-            period: {
-              year: post?.period?.year,
-              month: post?.period?.month,
-            },
-            costs: {
-              min: post?.costs?.min,
-              max: post?.costs?.max,
-              contract: post?.costs?.contract,
-              display: post?.costs?.display,
-              type: post?.costs?.type,
-            },
-            handles: post.handles?.[0]
-              ? post.handles.map((value) => ({
-                  handle: value,
-                }))
-              : [{ handle: "" }],
-            tools: post.tools?.[0]
-              ? post.tools.map((value) => ({
-                  tool: value,
-                }))
-              : [{ tool: "" }],
-            requires: post.requires?.[0]
-              ? post.requires.map((value) => ({
-                  require: value,
-                }))
-              : [{ require: "" }],
-            prefers: post.prefers?.[0]
-              ? post.prefers.map((value) => ({
-                  prefer: value,
-                }))
-              : [{ prefer: "" }],
-            adjustment: post?.adjustment,
-            interviews: post?.interviews,
-            times: post?.times,
-            remote: post?.remote,
-            distribution: post?.distribution,
-            span: post?.span,
-            note: post?.note,
-            status: post?.status,
-            memo: post?.memo,
-          }
-        : index === "resources" && {
-            display: post?.display,
-            name: post?.name,
-            roman: post?.roman,
-            position: post?.position,
-            sex: post?.sex,
-            age: post?.age,
-            body: post?.body,
-            belong: post?.belong,
-            station: post?.station,
-            period: {
-              year: post?.period?.year,
-              month: post?.period?.month,
-            },
-            costs: {
-              min: post?.costs?.min,
-              max: post?.costs?.max,
-              contract: post?.costs?.contract,
-              display: post?.costs?.display,
-              type: post?.costs?.type,
-            },
-            handles: post.handles?.[0]
-              ? post.handles.map((value) => ({
-                  handle: value,
-                }))
-              : [{ handle: "" }],
-            tools: post.tools?.[0]
-              ? post.tools.map((value) => ({
-                  tool: value,
-                }))
-              : [{ tool: "" }],
-            skills: post.skills?.[0]
-              ? post.skills.map((value) => ({
-                  skill: value,
-                }))
-              : [{ skill: "" }],
-            parallel: post?.parallel,
-            note: post?.note,
-            status: post?.status,
-            memo: post?.memo,
-          }
+        ? matters.defaultValues(post)
+        : index === "resources" && resources.defaultValues(post)
     );
   }, [index, methods, post]);
 
@@ -205,64 +36,11 @@ export const Post = ({ index, handleClose }) => {
       index === "matters"
         ? {
             ...post,
-            ...{
-              display: data.display,
-              title: data.title,
-              position: data.position,
-              body: data.body,
-              location: data.location,
-              period: data.period,
-              costs: data.costs,
-              adjustment: data.adjustment,
-              times: data.times,
-              handles: data.handles
-                .filter((object) => object[Object.keys(object)])
-                .map((object) => object[Object.keys(object)]),
-              tools: data.tools
-                .filter((object) => object[Object.keys(object)])
-                .map((object) => object[Object.keys(object)]),
-              requires: data.requires
-                .filter((object) => object[Object.keys(object)])
-                .map((object) => object[Object.keys(object)]),
-              prefers: data.prefers
-                .filter((object) => object[Object.keys(object)])
-                .map((object) => object[Object.keys(object)]),
-              interviews: data.interviews,
-              remote: data.remote,
-              distribution: data.distribution,
-              span: data.span,
-              note: data.note,
-              status: data.status,
-              memo: data.memo,
-            },
+            ...matters.object(data),
           }
         : index === "resources" && {
             ...post,
-            ...{
-              display: data.display,
-              roman: data.roman,
-              sex: data.sex,
-              age: data.age,
-              body: data.body,
-              belong: data.belong,
-              station: data.station,
-              period: data.period,
-              costs: data.costs,
-              position: data.position,
-              handles: data.handles
-                .filter((object) => object[Object.keys(object)])
-                .map((object) => object[Object.keys(object)]),
-              tools: data.tools
-                .filter((object) => object[Object.keys(object)])
-                .map((object) => object[Object.keys(object)]),
-              skills: data.skills
-                .filter((object) => object[Object.keys(object)])
-                .map((object) => object[Object.keys(object)]),
-              parallel: data.parallel,
-              note: data.note,
-              status: data.status,
-              memo: data.memo,
-            },
+            ...resources.object(data),
           };
 
     dispatch(postSlice.editPost({ index: index, post: object }));
