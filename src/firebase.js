79d11a3bcd7c636c9668df9706abd832da47baf6
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-18 14:46:17 +0900

    開発環境 実装

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 import "firebase/functions";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APPID,
+  appId: process.env.REACT_APP_APP_ID,
 });
 
 export const db = app.firestore();
