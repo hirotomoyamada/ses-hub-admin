852efe65b24323c1872cd52b732151cc6fdff815
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-14 16:14:42 +0900

    uid 実装

--- src/layouts/side/components/data/components/Auth.jsx
+++ src/layouts/side/components/data/components/Auth.jsx
@@ -1,10 +1,34 @@
 import styles from "../Data.module.scss";
 
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+import { useState } from "react";
+
 import { timestamp } from "../../../../../functions/timestamp";
 
 export const Auth = ({ user }) => {
+  const [copy, setCopy] = useState(false);
+
+  const handleCopy = () => {
+    setCopy(true);
+    setTimeout(() => setCopy(false), 2000);
+  };
+
   return (
     <div className={styles.data_col}>
+      <div className={styles.data_item}>
+        <span className={styles.data_item_tag}>UID</span>
+        <CopyToClipboard text={user?.uid} onCopy={handleCopy}>
+          <button
+            className={`${styles.data_item_text} ${styles.data_item_btn} ${
+              copy && styles.data_item_btn_copy
+            }`}
+          >
+            {user?.uid}
+          </button>
+        </CopyToClipboard>
+      </div>
+
       {user?.lastLogin && (
         <div className={styles.data_item}>
           <span className={styles.data_item_tag}>最終ログイン</span>
@@ -13,6 +37,7 @@ export const Auth = ({ user }) => {
           </span>
         </div>
       )}
+
       {user?.createAt && (
         <div className={styles.data_item}>
           <span className={styles.data_item_tag}>作成</span>
@@ -21,6 +46,7 @@ export const Auth = ({ user }) => {
           </span>
         </div>
       )}
+
       {user?.updateAt && (
         <div className={styles.data_item}>
           <span className={styles.data_item_tag}>更新</span>
