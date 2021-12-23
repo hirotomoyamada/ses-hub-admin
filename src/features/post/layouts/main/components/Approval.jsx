d5eea8698cf76a2cdcd98f7c20dcb328b2277649
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-12-23 17:43:22 +0900

    当日 から 当日中 に変更

--- src/features/post/layouts/main/components/Approval.jsx
+++ src/features/post/layouts/main/components/Approval.jsx
@@ -11,7 +11,7 @@ export const Approval = () => {
       <span className={root.main_tag}>稟議速度</span>
       <div className={`${styles.item} ${styles.item_select}`}>
         <select className={styles.item_input} {...register("approval")}>
-          <option value={"当日"}>当日</option>
+          <option value={"当日中"}>当日中</option>
           <option value={"翌営業1日以内"}>翌営業1日以内</option>
           <option value={"翌営業3日以内"}>翌営業3日以内</option>
           <option value={"不明"}>不明</option>
