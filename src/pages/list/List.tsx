ea79d019a726215625ffd5a749a2b6b4dc58c7b0
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-03-03 18:48:54 +0900

    usePosts hook 不具合 修正

--- src/pages/list/List.tsx
+++ src/pages/list/List.tsx
@@ -1,5 +1,5 @@
+import { usePosts } from "hooks/usePosts";
 import { useListScrollFetch } from "hooks/useListScrollFetch";
-
 import { Posts } from "./components/Posts";
 import { NotFound } from "./components/NotFound";
 import { Load } from "./components/Load";
@@ -17,7 +17,8 @@ interface PropType {
   posts?: Matter[] | Resource[] | Company[] | Person[];
 }
 
-export const List: React.FC<PropType> = ({ index, posts, search }) => {
+export const List: React.FC<PropType> = ({ index, search }) => {
+  const posts = usePosts(index, search);
   const [list, load, page, hit] = useListScrollFetch(index, search);
 
   return (
@@ -27,6 +28,7 @@ export const List: React.FC<PropType> = ({ index, posts, search }) => {
       ) : (
         <NotFound index={index} list={list} />
       )}
+
       {posts && posts.length >= 50 && (
         <Load load={load} page={page} hit={hit} />
       )}
