b3af8a10875f317754cf85cdc2016f3684c4962f
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-03-03 18:49:33 +0900

    usePosts 不具合 修正

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -14,39 +14,41 @@ export const usePosts = (
     type: string | null;
     filter: string | null;
   }
-): Matter[] | Resource[] | Company[] | Person[] | undefined => {
+): Matter[] | Resource[] | Company[] | Person[] | [] => {
   const dispatch = useDispatch();
 
   const index = page;
 
   if (
-    index === "matters" ||
-    index === "resources" ||
-    index === "companys" ||
-    index === "persons"
+    index !== "matters" &&
+    index !== "resources" &&
+    index !== "companys" &&
+    index !== "persons"
   ) {
-    const posts = useSelector(postSlice.posts)[index];
-
-    useEffect(() => {
-      dispatch(
-        fetchPosts({
-          index: index,
-          value: search.value,
-          target: search.target,
-          type: search.type,
-          filter: search.filter,
-          fetch: posts.length ? true : false,
-        })
-      );
-    }, [
-      dispatch,
-      index,
-      search.value,
-      search.target,
-      search.type,
-      search.filter,
-    ]);
-
-    return posts;
+    return [];
   }
+
+  const posts = useSelector(postSlice.posts)[index];
+
+  useEffect(() => {
+    dispatch(
+      fetchPosts({
+        index: index,
+        value: search.value,
+        target: search.target,
+        type: search.type,
+        filter: search.filter,
+        fetch: posts.length ? true : false,
+      })
+    );
+  }, [
+    dispatch,
+    index,
+    search.value,
+    search.target,
+    search.type,
+    search.filter,
+  ]);
+
+  return posts;
 };
