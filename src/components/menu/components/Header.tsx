214133cc442a1b4aecdd661e446d93adc09b0652
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-05-09 14:08:29 +0900

    ダッシュボードへ遷移

--- src/components/menu/components/Header.tsx
+++ src/components/menu/components/Header.tsx
@@ -1,14 +1,17 @@
 import styles from "../Menu.module.scss";
+import { useNavigate } from "react-router-dom";
 
 export const Header: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.menu_header}>
+    <button className={styles.menu_header} onClick={() => navigate("/")}>
       <img
         src={`${process.env.PUBLIC_URL}/img/logo/logo.svg`}
         alt=""
         className={styles.menu_header_logo}
       />
       <span>管理画面</span>
-    </div>
+    </button>
   );
 };
