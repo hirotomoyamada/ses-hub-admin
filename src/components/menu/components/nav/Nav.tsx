ab28d4a123a0866fa2f97440905343d7f86ebf89
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-05-09 14:08:46 +0900

    ダッシュボード 追加

--- src/components/menu/components/nav/Nav.tsx
+++ src/components/menu/components/nav/Nav.tsx
@@ -13,21 +13,28 @@ import { faCcStripe } from "@fortawesome/free-brands-svg-icons";
 import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 export const Nav: React.FC = () => {
   return (
     <nav className={styles.menu_nav}>
       <div className={styles.menu_nav_wrap}>
+        <Header index="" icon={faHome as IconProp} text="ダッシュボード" />
+      </div>
+
+      <div className={styles.menu_nav_wrap}>
         <Header index={["companys", "persons"]} text="ユーザー" />
         <Btn index="companys" text="メンバー" />
         <Btn index="persons" text="エンジニア" />
       </div>
+
       <div className={styles.menu_nav_wrap}>
         <Header index={["matters", "persons"]} text="投稿" />
         <Btn index="matters" text="案件" />
         <Btn index="resources" text="人材" />
       </div>
+
       <div className={styles.menu_nav_wrap}>
         <Header icon={faServer as IconProp} text="サーバー" />
         <Link icon={faDatabase as IconProp} text="Firebase" />
@@ -35,6 +42,7 @@ export const Nav: React.FC = () => {
         <Link icon={faCcStripe as IconProp} text="Stripe" />
         <Link icon={faAt as IconProp} text="SendGrid" />
       </div>
+
       <div className={styles.menu_nav_wrap}>
         <Header icon={faCog as IconProp} text="設定" />
         <Btn index="setting" icon={faSlidersH as IconProp} text="全般" />
--- src/components/menu/components/nav/components/Header.tsx
+++ src/components/menu/components/nav/components/Header.tsx
@@ -1,5 +1,5 @@
 import styles from "../../../Menu.module.scss";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolder } from "@fortawesome/free-solid-svg-icons";
@@ -7,19 +7,36 @@ import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 interface PropType {
-  index?: string[];
+  index?: string | string[];
   icon?: IconProp;
   text: string;
 }
 
 export const Header: React.FC<PropType> = ({ index, icon, text }) => {
+  const navigate = useNavigate();
   const pathname = useLocation().pathname.slice(1);
   const page = pathname.substring(
     0,
     pathname.indexOf("/") >= 0 ? pathname.indexOf("/") : undefined
   );
 
-  return (
+  return typeof index === "string" ? (
+    <button
+      className={`
+        ${styles.menu_nav_head}
+        ${styles.menu_nav_head_btn}
+        ${page === index && styles.menu_nav_head_active}
+      `}
+      onClick={() => navigate(`/${index}`)}
+    >
+      <FontAwesomeIcon
+        icon={
+          icon || (index && index.indexOf(page) >= 0 ? faFolderOpen : faFolder)
+        }
+      />
+      {text}
+    </button>
+  ) : (
     <div className={styles.menu_nav_head}>
       <FontAwesomeIcon
         icon={
