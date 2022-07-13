f080e46c0bd8b1b6818284766613c88c9aaa9578
hirotomo.yamada <combined_basilisk@pfg.app>
2022-07-13 19:13:53 +0900

    console.log 削除

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -15,8 +15,6 @@ import {
 
 import { getFunctions } from "firebase/functions";
 
-console.log(process.env);
-
 const app = initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
