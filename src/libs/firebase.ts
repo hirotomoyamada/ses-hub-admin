0a21de9574d82b3f9a3f418fb041aae02849b531
hirotomo.yamada <combined_basilisk@pfg.app>
2022-07-13 19:12:00 +0900

    env 修正

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -15,8 +15,10 @@ import {
 
 import { getFunctions } from "firebase/functions";
 
+console.log(process.env);
+
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
