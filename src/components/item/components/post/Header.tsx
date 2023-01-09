2e4be4beeb4a4b1be4a791a1d84f2b3a2918f139
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-01-09 16:01:43 +0900

    add 業界

--- src/components/item/components/post/Header.tsx
+++ src/components/item/components/post/Header.tsx
@@ -1,7 +1,7 @@
-import styles from "./Post.module.scss";
+import styles from './Post.module.scss';
 
-import * as functions from "functions";
-import { Matter, Resource } from "types/post";
+import * as functions from 'functions';
+import { Matter, Resource } from 'types/post';
 
 interface PropType {
   post: Matter | Resource;
@@ -14,52 +14,57 @@ export const Header: React.FC<PropType> = ({ post, min }) => {
       <div className={styles.item_row}>
         <div
           className={`${styles.item_category} ${
-            post?.display === "public"
+            post?.display === 'public'
               ? styles.item_category_public
-              : post?.display === "private" && styles.item_category_private
-          }`}
-        >
+              : post?.display === 'private' && styles.item_category_private
+          }`}>
           <span>
-            {post?.display === "public"
-              ? "公開"
-              : post?.display === "private" && "非公開"}
+            {post?.display === 'public'
+              ? '公開'
+              : post?.display === 'private' && '非公開'}
           </span>
         </div>
 
         <div
           className={`${styles.item_category} ${styles.item_category_status} ${
-            post?.status === "保留中"
+            post?.status === '保留中'
               ? styles.item_category_status_reserve
-              : post?.status === "提案中"
+              : post?.status === '提案中'
               ? styles.item_category_status_propo
-              : post?.status === "面談中"
+              : post?.status === '面談中'
               ? styles.item_category_status_interview
-              : post?.status === "フォロー中"
+              : post?.status === 'フォロー中'
               ? styles.item_category_status_follow
-              : post?.status === "成約" && styles.item_category_status_end
-          }`}
-        >
+              : post?.status === '成約' && styles.item_category_status_end
+          }`}>
           <span>{post?.status}</span>
         </div>
 
+        {'industry' in post && post?.industry ? (
+          <div
+            className={`${styles.item_category} ${styles.item_category_industry}`}>
+            <span>{post?.industry}</span>
+          </div>
+        ) : null}
+
         <div
-          className={`${styles.item_category} ${styles.item_category_position}`}
-        >
+          className={`${styles.item_category} ${styles.item_category_position}`}>
           <span>{post?.position}</span>
         </div>
       </div>
 
-      <div className={styles.item_row}>
-        <span className={styles.item_time}>
-          作成：{functions.root.timestamp(post?.createAt)}
-        </span>
+      {!min ? (
+        <div className={styles.item_row}>
+          <span className={styles.item_time}>
+            作成：{functions.root.timestamp(post?.createAt)}
+          </span>
 
-        <span className={styles.item_time}>
-          {post?.updateAt &&
-            !min &&
-            `更新：${functions.root.timestamp(post?.updateAt)}`}
-        </span>
-      </div>
+          <span className={styles.item_time}>
+            {post?.updateAt &&
+              `更新：${functions.root.timestamp(post?.updateAt)}`}
+          </span>
+        </div>
+      ) : null}
     </div>
   );
 };
