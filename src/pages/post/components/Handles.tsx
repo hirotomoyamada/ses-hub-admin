9efeb309c06d2aaf225d47c30fe2ee7341c105ca
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-01-09 13:49:10 +0900

    change 言語 to 言語・フレームワーク

--- src/pages/post/components/Handles.tsx
+++ src/pages/post/components/Handles.tsx
@@ -1,12 +1,12 @@
-import styles from "./Item.module.scss";
-import root from "../Post.module.scss";
+import styles from './Item.module.scss';
+import root from '../Post.module.scss';
 
-import AddIcon from "@material-ui/icons/Add";
-import RemoveIcon from "@material-ui/icons/Remove";
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 
-import { useFormContext, useFieldArray } from "react-hook-form";
-import { Data } from "pages/post/Post";
-import { Index } from "features/root/initialState";
+import { useFormContext, useFieldArray } from 'react-hook-form';
+import { Data } from 'pages/post/Post';
+import { Index } from 'features/root/initialState';
 
 interface PropType {
   index: Index;
@@ -25,7 +25,7 @@ export const Handles: React.FC<PropType> = ({ index }) => {
     remove: handlesRemove,
   } = useFieldArray({
     control,
-    name: "handles",
+    name: 'handles',
   });
 
   return (
@@ -34,22 +34,22 @@ export const Handles: React.FC<PropType> = ({ index }) => {
         {handlesFields.map((field, i) => (
           <div key={field.id} className={styles.item}>
             <input
-              placeholder="言語"
+              placeholder="言語・フレームワーク"
               className={`${styles.item_input} ${
                 errors.handles?.[i]?.handle && styles.item_input_error
               }`}
               {...register(`handles.${i}.handle` as const, {
                 pattern: {
                   value: /^\S+/,
-                  message: "先頭にスペースは使えません",
+                  message: '先頭にスペースは使えません',
                 },
                 minLength: {
                   value: 2,
-                  message: "2文字以上で入力してください",
+                  message: '2文字以上で入力してください',
                 },
                 maxLength: {
                   value: 16,
-                  message: "16文字以内で入力してください",
+                  message: '16文字以内で入力してください',
                 },
               })}
             />
@@ -58,26 +58,23 @@ export const Handles: React.FC<PropType> = ({ index }) => {
               {i !== 0 && (
                 <button
                   className={styles.item_btn_remove}
-                  onClick={() => handlesRemove(i)}
-                >
+                  onClick={() => handlesRemove(i)}>
                   <RemoveIcon className={styles.item_btn_icon} />
                 </button>
               )}
-              {index === "matters" && i === handlesFields.length - 1 && i < 4 && (
+              {index === 'matters' && i === handlesFields.length - 1 && i < 4 && (
                 <button
                   className={styles.item_btn_add}
-                  onClick={() => handlesAppend({ handle: "" })}
-                >
+                  onClick={() => handlesAppend({ handle: '' })}>
                   <AddIcon className={styles.item_btn_icon} />
                 </button>
               )}
-              {index === "resources" &&
+              {index === 'resources' &&
                 i === handlesFields.length - 1 &&
                 i < 9 && (
                   <button
                     className={styles.item_btn_add}
-                    onClick={() => handlesAppend({ handle: "" })}
-                  >
+                    onClick={() => handlesAppend({ handle: '' })}>
                     <AddIcon className={styles.item_btn_icon} />
                   </button>
                 )}
