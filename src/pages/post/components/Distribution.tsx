eda6ae0e50f3600454640f7b84cbcb583fe0ef0f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-09-12 14:41:03 +0900

    refactor(post): simplify select option rendering in Distribution.tsx

--- src/pages/post/components/Distribution.tsx
+++ src/pages/post/components/Distribution.tsx
@@ -1,8 +1,8 @@
-import styles from "./Item.module.scss";
-import root from "../Post.module.scss";
+import styles from './Item.module.scss';
+import root from '../Post.module.scss';
 
-import { useFormContext } from "react-hook-form";
-import { Data } from "functions/_matter";
+import { useFormContext } from 'react-hook-form';
+import { Data } from 'functions/_matter';
 
 export const Distribution: React.FC = () => {
   const {
@@ -15,27 +15,23 @@ export const Distribution: React.FC = () => {
       <span className={root.post_tag}>商流</span>
       <div className={`${styles.item} ${styles.item_select}`}>
         <select
-          className={`${styles.item_input} ${
-            errors.distribution && styles.item_input_error
-          }`}
-          {...register("distribution", {
+          className={`${styles.item_input} ${errors.distribution && styles.item_input_error}`}
+          {...register('distribution', {
             required: {
               value: true,
-              message: "商流を選択してください",
+              message: '商流を選択してください',
             },
-          })}
-        >
-          <option value={"プライム"}>プライム</option>
-          <option value={"二次請け"}>二次請け</option>
-          <option value={"三次請け"}>三次請け</option>
-          <option value={"営業支援"}>営業支援</option>
-          <option value={"その他"}>その他</option>
+          })}>
+          <option value={'プライム'}>プライム</option>
+          <option value={'二次請け'}>二次請け</option>
+          <option value={'三次請け'}>三次請け</option>
+          <option value={'営業支援'}>営業支援</option>
+          <option value={'SIer案件'}>SIer案件</option>
+          <option value={'その他'}>その他</option>
         </select>
 
         {errors?.distribution?.message && (
-          <span className={styles.item_error}>
-            {errors.distribution.message}
-          </span>
+          <span className={styles.item_error}>{errors.distribution.message}</span>
         )}
       </div>
     </div>
